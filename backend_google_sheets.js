bc7195a1d341825f2e10979ed6a6da3a5a919e21
AndrielRox <andrielrox1999@outlook.com>
2025-02-27 16:56:33 -0300

    Add files via upload

--- backend_google_sheets.js
+++ backend_google_sheets.js
@@ -43,5 +43,5 @@ app.post("/api/update-truck-location", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
